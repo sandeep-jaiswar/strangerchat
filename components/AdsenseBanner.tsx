a0885935ebbafc1954b6c91dc1ae9180089af1dd
sandeep-jaiswar <jaiswarsandeep119@gmail.com>
2025-11-25 14:17:24 +0530

    feat: update AdsenseBanner component to include adClient in script URL

--- components/AdsenseBanner.tsx
+++ components/AdsenseBanner.tsx
@@ -32,7 +32,8 @@ export const AdsenseBanner: React.FC<AdsenseBannerProps> = ({ adClient }) => {
   return (
     <>
       <Script
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+        async
+        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${adClient}`}
         strategy="afterInteractive"
         crossOrigin="anonymous"
       />
