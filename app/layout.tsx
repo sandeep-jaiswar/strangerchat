bd22e5bcdcd38bea98088b279309b8fe64e485c2
sandeep-jaiswar <jaiswarsandeep119@gmail.com>
2025-11-25 14:40:27 +0530

    feat: enhance RootLayout with Google AdSense site verification meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
 import "styles/tailwind.css"
+import { env } from "../env.mjs"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
-        {/* Google AdSense */}
+        {/* Google AdSense site verification */}
+        <meta name="google-adsense-account" content={env.ADSENSE_CLIENT} />
+        {/* Google AdSense script */}
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
